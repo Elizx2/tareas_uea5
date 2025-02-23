d99e190053eb07d7344494a64330da4298317f1b
Elsaluz andrade <eliz.lux.andr@gmail.com.com>
2025-02-23 17:24:00 -0600

    tareas

---
+++ variables/script.js
@@ -0,0 +1,8 @@
+// variables
+
+let ingrediente1 = "azucar"
+let precio = 120;
+
+//document.write(ingrediente1);
+document.getElementById ('demo').innerHTML="un ingrediente necesario es: "+ ingrediente1;
+document.getElementById ('segu').innerHTML="el precio del "+ ingrediente1 + " es de: "+ precio + " pesos";
